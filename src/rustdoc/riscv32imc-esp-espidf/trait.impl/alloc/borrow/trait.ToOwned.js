6aa47f79ec91023a52ef7b32c2c7b8c82cbbf069
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2024-03-25 08:45:02 +0300

    update docs

---
+++ src/rustdoc/riscv32imc-esp-espidf/trait.impl/alloc/borrow/trait.ToOwned.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"serde_json":[["impl ToOwned for <a class=\"struct\" href=\"serde_json/value/struct.RawValue.html\" title=\"struct serde_json::value::RawValue\">RawValue</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ src/rustdoc/riscv32imc-esp-espidf/trait.impl/std/io/trait.Write.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"bytes":[["impl&lt;B: <a class=\"trait\" href=\"bytes/buf/trait.BufMut.html\" title=\"trait bytes::buf::BufMut\">BufMut</a> + Sized&gt; Write for <a class=\"struct\" href=\"bytes/buf/struct.Writer.html\" title=\"struct bytes::buf::Writer\">Writer</a>&lt;B&gt;"]],
+"futures_util":[["impl&lt;T&gt; Write for <a class=\"struct\" href=\"futures_util/io/struct.AllowStdIo.html\" title=\"struct futures_util::io::AllowStdIo\">AllowStdIo</a>&lt;T&gt;<div class=\"where\">where\n    T: Write,</div>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
